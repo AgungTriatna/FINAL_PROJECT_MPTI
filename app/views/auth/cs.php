08e1ab8a3f6b3297d7b78043c16eb613b19ee24b
wsaji <wahyusetioaji775@gmail.com>
2024-06-15 11:27:05 +0700

    sprint 2 - login front end

---
+++ app/views/auth/cs.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="base_url" content="<?= BASEURL ?>">
+
+    <title><?= APP_NAME ?> - Landing</title>
+    <link rel="icon" href="<?= BASEURL ?>/favicon.ico" type="image/png">
+
+
+    <!-- UI -->
+    <link rel="stylesheet" href="<?= BASEURL ?>/light/css/app-light.css" id="lightTheme">
+
+    <!-- Fonts CSS -->
+    <link href="https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <!-- Icons CSS -->
+    <link rel="stylesheet" href="<?= BASEURL ?>/light/css/feather.css">
+
+    <!-- Date Range Picker CSS -->
+    <link rel="stylesheet" href="<?= BASEURL ?>/light/css/daterangepicker.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css">
+
+</head>
+
+<body style="padding: 5% 10%;">
+
+
+    <h3>Hubungi Kami</h3>
+    <hr>
+
+    <main>
+        <div class="row justify-content-center">
+            <div class="col-10 col-lg-10 col-xl-12 mx-auto">
+                <div class="row mt-4">
+
+                    <div class="col-md-3">
+
+                        <div class="card shadow mb-4">
+                            <div class="card-body">
+                                <div class="row align-items-center">
+                                    <div class="col-3 text-center">
+                                        <span class="circle circle-md bg-primary">
+                                            <span class="fe fe-mail fe-24 text-white"></span>
+                                        </span>
+                                    </div>
+                                    <div class="col">
+                                        <h3 class="h6 mb-0 text-uppercase">E-Mail</h3>
+                                        <p class="small text-muted mb-0">admin@advics-min.co.id</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                    <div class="col-md-3">
+                        <div class="card shadow mb-4">
+                            <div class="card-body">
+                                <div class="row align-items-center">
+                                    <div class="col-3 text-center">
+                                        <span class="circle circle-md bg-success">
+                                            <span class="fe fe-phone fe-24 text-white"></span>
+                                        </span>
+                                    </div>
+                                    <div class="col">
+                                        <h3 class="h6 mb-0 text-uppercase">Phone</h3>
+                                        <p class="small text-muted mb-0">021-222-222</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="col-md-3">
+                        <div class="card shadow mb-4">
+                            <div class="card-body">
+                                <div class="row align-items-center">
+                                    <div class="col-3 text-center">
+                                        <span class="circle circle-md bg-success">
+                                            <span class="fe fe-phone fe-24 text-white"></span>
+                                        </span>
+                                    </div>
+                                    <div class="col">
+                                        <h3 class="h6 mb-0 text-uppercase">Fax</h3>
+                                        <p class="small text-muted mb-0">021-222-222</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="col-md-3">
+                        <div class="card shadow mb-4">
+                            <div class="card-body">
+                                <div class="row align-items-center">
+                                    <div class="col-3 text-center">
+                                        <span class="circle circle-md bg-warning">
+                                            <span class="fe fe-message-circle fe-24 text-white"></span>
+                                        </span>
+                                    </div>
+                                    <div class="col">
+                                        <h3 class="h6 mb-0 text-uppercase">Message</h3>
+                                        <p class="small text-muted mb-0">0838-2424-000</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <img src="<?= BASEURL ?>/light/assets/avatars/lupa.png" alt="logo" width="100%">
+
+
+                </div> <!-- end section -->
+
+            </div </main> <!-- main -->
+</body>
+
+</html>
